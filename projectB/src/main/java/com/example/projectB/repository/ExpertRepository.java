43a598a1f977a1e9c97d196bd4ba577479c1871f
RuohanCheng <15262829659@163.com>
2026-05-03 19:43:09 +0800

    docs: remove projectB from main

--- projectB/src/main/java/com/example/projectB/repository/ExpertRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.projectB.repository;
-
-import com.example.projectB.entity.Expert;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ExpertRepository extends JpaRepository<Expert, Long> {
-}
