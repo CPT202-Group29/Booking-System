58ad2a472ee9174d0847bad1a20fba3d8ff83eb6
Jundi.Jing <15051130661@163.com>
2026-05-08 04:29:15 +0800

    Delete src/main/java/com/booking directory

--- src/main/java/com/booking/repository/ExpertiseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.booking.repository;
-
-import com.booking.entity.Expertise;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ExpertiseRepository extends JpaRepository<Expertise, Integer> {
-}
